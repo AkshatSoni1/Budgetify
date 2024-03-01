1d8943bf8c8f5868ce775704984f18a232263e1e
AkshatSoni1 <sakshat551@gmail.com>
2024-03-01 17:05:47 +0530

    Navbar colour fixed

--- components/Modals/AddModal.jsx
+++ components/Modals/AddModal.jsx
@@ -4,7 +4,7 @@ import ShowToast from '@/helper/page';
 import { useContext, useState } from 'react'
 
 const AddModal = () => {
-    const { addToggle, setAddToggle, user, setCount, mode } = useContext(AppContext);
+    const { addToggle, setAddToggle, user, setCount } = useContext(AppContext);
     const [bName, setBName] = useState('')
     const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -47,16 +47,16 @@ const AddModal = () => {
             <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className={`${!addToggle && "hidden"} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 w-full max-w-md max-h-full">
                     {/* <!-- Modal content --> */}
-                    <div className={`relative bg-white rounded-lg shadow ${mode==='dark' && "dark:bg-gray-700"}`}>
+                    <div className={`relative bg-white rounded-lg shadow`}>
                         {/* <!-- Modal header --> */}
-                        <div className={`flex items-center justify-between p-4 md:p-5 border-b rounded-t ${mode==='dark' && "dark:border-gray-600"}`}>
-                            <h3 className={`text-xl font-semibold text-gray-900 ${mode==='dark' && "dark:text-white"}`}>
+                        <div className={`flex items-center justify-between p-4 md:p-5 border-b rounded-t `}>
+                            <h3 className={`text-xl font-semibold text-gray-900 `}>
                                 Add a budget
                             </h3>
                             <button onClick={() => {
                                 setAddToggle((addToggle) => !addToggle)
                                 setBName('')
-                                }} type="button" className={`end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center ${mode==='dark' && "dark:hover:bg-gray-600 dark:hover:text-white"}`} data-modal-hide="authentication-modal">
+                                }} type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="authentication-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
@@ -67,14 +67,14 @@ const AddModal = () => {
                         <div className="p-4 md:p-5">
                             <form className="space-y-4" onSubmit={handleSubmit}>
                                 <div>
-                                    <label htmlFor="name" className={`block mb-2 text-md text-gray-900 ${mode==='dark' && "dark:text-white"} font-light`}>Specify the name of the budget</label>
+                                    <label htmlFor="name" className='block mb-2 text-md text-gray-900 font-light'>Specify the name of the budget</label>
                                     <input 
                                     value={bName}
                                     onChange={(e)=>{setBName(e.target.value)}}
                                     type="text" 
                                     name="name" 
                                     id="name" 
-                                    className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${mode==='dark' && "dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"}`}
+                                    className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5`}
                                     required
                                     />
                                 </div>
